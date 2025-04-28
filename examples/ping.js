979f8360cc0bcb5f124203f2be33a07375eeac5d
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2025-04-28 15:14:18 +0200

    Debugging

--- examples/ping.js
+++ examples/ping.js
@@ -6,12 +6,12 @@ export const options = {
     duration: "30s",
 };
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'http://127.0.0.1:8080';
 
 export default function () {
     const resp = http.get(`${baseUrl}/ping`);
     if (resp.status !== 200) {
-        failure(`Request to ping failed with status ${resp.status} (body: ${resp.body})`);
+        failure(`Request to ping failed with status ${resp.status}`);
     }
 
     success();
