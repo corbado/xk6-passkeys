9138617a9df35471eb5ecdb997462eed0537e7d5
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2025-04-28 14:59:01 +0200

    Implemented ping endpoint

---
+++ examples/ping.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { success, failure } from './helper.js';
+
+export const options = {
+    vus: 2,
+    duration: "30s",
+};
+
+const baseUrl = 'http://localhost:8080';
+
+export default function () {
+    const resp = http.get(`${baseUrl}/ping`);
+    if (resp.status !== 200) {
+        failure(`Request to ping failed with status ${resp.status} (body: ${resp.body})`);
+    }
+
+    success();
+}
